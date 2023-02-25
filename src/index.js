7fa24f001343060a30fdaa4b31ab1d3dbc6f37c8
myvicc <dir@acg.co.ua>
2023-02-25 19:08:54 +0200

    added file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Application from "./Application";
+
+
+const root = createRoot(document.querySelector('#root'));
+root.render(
+  <React.StrictMode>
+    <div>
+        <Application />
+    </div>
+  </React.StrictMode>
+);
